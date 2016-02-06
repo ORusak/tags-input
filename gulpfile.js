c1c76a4342f932bfb102528c79d092cfc9acfd78
Oleg Rusak <hare.brown@gmail.com>
2016-02-06 20:56:38 +0300

    add task deploy source for example

--- gulpfile.js
+++ gulpfile.js
@@ -13,9 +13,25 @@ var rename          = require('gulp-rename');
 gulp.task('docs', function () {
     return gulp.src('app/*.js')
         .pipe(concat('README.md'))
-        .pipe(gulpJsdoc2md({ template: fs.readFileSync('./jsdoc2md/readme.hbs', 'utf8') }))
+        .pipe(gulpJsdoc2md({ template: fs.readFileSync('./jsdoc2md/README.hbs', 'utf8') }))
         .on('error', function (err) {
             gutil.log(gutil.colors.red('jsdoc2md failed'), err.message)
         })
         .pipe(gulp.dest('./'))
 });
+
+gulp.task('example', function (callback) {
+    gulp.src('app/*')
+        .pipe(gulp.dest('example/plugin-tags-input'))
+        .on('error', function (err) {
+            gutil.log(gutil.colors.red('copy plugin tags-input failed'), err.message);
+        });
+
+    gulp.src('node_modules/javascript-auto-complete/auto-complete.*')
+        .pipe(gulp.dest('example/javascript-auto-complete'))
+        .on('error', function (err) {
+            gutil.log(gutil.colors.red('copy plugin autocomplite failed'), err.message);
+        });
+});
+
+gulp.task('default', ['docs', 'example']);
